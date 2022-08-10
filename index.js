7100004c81b8f57b364b54380ff50d71daa9244c
Arif Nur <nnarif37@gmail.com>
2022-08-10 10:16:11 +0700

    new port to 3000

--- index.js
+++ index.js
@@ -1,9 +1,9 @@
 const app = require('express')();
 
-const port = 80;
-
-app.get("", (req, res) => {
-    res.send("hello world war 2");
-});
+const port = 3000;
 
 app.listen(port, () => console.log(`Listen on port ${port}`));
+
+app.get("/", (req, res) => {
+    res.send("hello world war 2");
+});
