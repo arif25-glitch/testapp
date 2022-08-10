39e1c6499379c528dc734cb2b6745dbfb64fa0fa
Arif Nur <nnarif37@gmail.com>
2022-08-10 10:13:22 +0700

    new index.js

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ const app = require('express')();
 const port = process.env.PORT || 3000;
 
 app.get("", (req, res) => {
-    res.send("hello world");
+    res.send("hello world war 2");
 });
 
 app.listen(port, () => console.log(`Listen on port ${port}`));
