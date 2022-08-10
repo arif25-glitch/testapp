600c720d2c8db8dea132c0bf11fd22c278c889aa
Arif Nur <nnarif37@gmail.com>
2022-08-10 10:08:12 +0700

    new version

--- index.js
+++ index.js
@@ -1,9 +1,9 @@
 const app = require('express')();
 
-const port = 80;
+const port = process.env.PORT || 3000;
 
-app.get('/', (req, res) => {
-    res.end("Hello There");
+app.get("", (req, res) => {
+    res.send("hello world");
 });
 
 app.listen(port, () => console.log(`Listen on port ${port}`));
